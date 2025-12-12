09ea2a863bc13d04a840b433ecb6e45a34380002
SAMKIEL <samuelezekiel488@gmail.com>
2025-12-12 19:27:28 +0100

    fix: correct syntax error in axios baseURL assignment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL", // We go replace this when dem give us the backend api endpoint
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // We go replace this when dem give us the backend api endpoint
   headers: {
     "Content-Type": "application/json",
   },
