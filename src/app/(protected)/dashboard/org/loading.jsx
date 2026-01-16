9a4dfd1b7b8250a146bae7118853b07c41d4bf4a
SAMKIEL <samuelezekiel488@gmail.com>
2026-01-16 14:54:30 +0100

    feat: add loading skeleton for the dashboard organization page

--- src/app/(protected)/dashboard/org/loading.jsx
+++ src/app/(protected)/dashboard/org/loading.jsx
@@ -1,9 +1,5 @@
-import { AnalyticsSkeleton } from "@/components/skeletons";
+import { OrganizerDashboardSkeleton } from "@/components/skeletons";
 
 export default function DashboardLoading() {
-  return (
-    <div className="min-h-screen p-4 md:p-8 max-w-7xl mx-auto">
-      <AnalyticsSkeleton />
-    </div>
-  );
+  return <OrganizerDashboardSkeleton />;
 }
